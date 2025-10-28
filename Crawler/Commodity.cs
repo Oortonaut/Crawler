5ca8ca1749cbfae21b68e568fd61d2e1484d87f0
Ace Stapp <acy.stapp@gmail.com>
2025-10-27 22:58:36 -0400

    Rejigger commodity values to match game stage

--- Crawler/Commodity.cs
+++ Crawler/Commodity.cs
@@ -87,7 +87,7 @@ public static class CommodityEx {
         new CommodityData(4.0f, 1.0f, 0.08f, CommodityFlag.Integral, GameTier.Early, CommodityCategory.Essential), // Crew
         new CommodityData(15f, 0.0f, 0.0f, CommodityFlag.Integral, GameTier.Early, CommodityCategory.Essential), // Morale
         new CommodityData(3.5f, 1.0f, 0.08f, CommodityFlag.Integral, GameTier.Early, CommodityCategory.Consumer), // Passengers
-        new CommodityData(5.0f, 1.2f, 0.10f, CommodityFlag.Integral, GameTier.Mid, CommodityCategory.Dangerous), // Soldiers
+        new CommodityData(30f, 1.2f, 0.10f, CommodityFlag.Integral, GameTier.Mid, CommodityCategory.Dangerous), // Soldiers
 
         // Life support
         new CommodityData(1f, 0.1f, 0.087f, CommodityFlag.None, GameTier.Early, CommodityCategory.Essential), // Air
@@ -101,36 +101,36 @@ public static class CommodityEx {
 
         // refined
         new CommodityData(30f, 0.1f, 0.25f, CommodityFlag.None, GameTier.Early, CommodityCategory.Refined), // Metal
-        new CommodityData(18.75f, 0.1f, 0.075f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Refined), // Chemicals
+        new CommodityData(100f, 0.1f, 0.075f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Refined), // Chemicals
         new CommodityData(11f, 0.1f, 0.065f, CommodityFlag.None, GameTier.Early, CommodityCategory.Refined), // Glass
 
         // Parts
-        new CommodityData(225f, 1.0f, 0.75f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Parts), // Ceramics
-        new CommodityData(175f, 1.0f, 0.4f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Parts), // Polymers
-        new CommodityData(425f, 1.0f, 2.0f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Parts), // Alloys
-        new CommodityData(135f, 1.0f, 0.6f, CommodityFlag.None, GameTier.Late, CommodityCategory.Parts), // Electronics
-        new CommodityData(430f, 1.0f, 0.7f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Dangerous), // Explosives
+        new CommodityData(1500f, 1.0f, 0.75f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Parts), // Ceramics
+        new CommodityData(1200f, 1.0f, 0.4f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Parts), // Polymers
+        new CommodityData(3000f, 1.0f, 2.0f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Parts), // Alloys
+        new CommodityData(2800f, 1.0f, 0.6f, CommodityFlag.None, GameTier.Late, CommodityCategory.Parts), // Electronics
+        new CommodityData(2800f, 1.0f, 0.7f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Dangerous), // Explosives
 
         // Consumer Goods
-        new CommodityData(4f, 0.001f, 0.0005f, CommodityFlag.Perishable, GameTier.Mid, CommodityCategory.Consumer), // Medicines
-        new CommodityData(165f, 1.0f, 0.2f, CommodityFlag.None, GameTier.Early, CommodityCategory.Luxury), // Textiles
-        new CommodityData(20f, 0.001f, 0.002f, CommodityFlag.None, GameTier.Late, CommodityCategory.Luxury), // Gems
-        new CommodityData(150f, 1.0f, 0.4f, CommodityFlag.None, GameTier.Early, CommodityCategory.Consumer), // Toys
-        new CommodityData(550f, 1.0f, 0.75f, CommodityFlag.None, GameTier.Late, CommodityCategory.Consumer), // Machines
-        new CommodityData(150.0f, 0.025f, 0.04f, CommodityFlag.Integral, GameTier.Late, CommodityCategory.Dangerous), // AiCores
-        new CommodityData(35f, 0.001f, 0.00035f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Luxury), // Media
+        new CommodityData(25f, 0.001f, 0.0005f, CommodityFlag.Perishable, GameTier.Mid, CommodityCategory.Consumer), // Medicines
+        new CommodityData(200f, 1.0f, 0.2f, CommodityFlag.None, GameTier.Early, CommodityCategory.Luxury), // Textiles
+        new CommodityData(800f, 0.001f, 0.002f, CommodityFlag.None, GameTier.Late, CommodityCategory.Luxury), // Gems
+        new CommodityData(180f, 1.0f, 0.4f, CommodityFlag.None, GameTier.Early, CommodityCategory.Consumer), // Toys
+        new CommodityData(18000f, 1.0f, 0.75f, CommodityFlag.None, GameTier.Late, CommodityCategory.Consumer), // Machines
+        new CommodityData(5000f, 0.025f, 0.04f, CommodityFlag.Integral, GameTier.Late, CommodityCategory.Dangerous), // AiCores
+        new CommodityData(200f, 0.001f, 0.00035f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Luxury), // Media
 
         // Vice & Contraband
         new CommodityData(3.75f, 0.01f, 0.01f, CommodityFlag.Bulky, GameTier.Early, CommodityCategory.Vice), // Liquor
-        new CommodityData(12f, 0.001f, 0.0005f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Vice), // Stims
-        new CommodityData(10f, 0.001f, 0.0005f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Vice), // Downers
-        new CommodityData(36f, 0.001f, 0.0006f, CommodityFlag.None, GameTier.Late, CommodityCategory.Vice), // Trips
-        new CommodityData(5.625f, 0.01f, 0.0075f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Dangerous), // SmallArms
+        new CommodityData(80f, 0.001f, 0.0005f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Vice), // Stims
+        new CommodityData(70f, 0.001f, 0.0005f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Vice), // Downers
+        new CommodityData(1200f, 0.001f, 0.0006f, CommodityFlag.None, GameTier.Late, CommodityCategory.Vice), // Trips
+        new CommodityData(35f, 0.01f, 0.0075f, CommodityFlag.None, GameTier.Mid, CommodityCategory.Dangerous), // SmallArms
 
         // Religious items
-        new CommodityData(6.0f, 0.003f, 0.004f, CommodityFlag.Integral, GameTier.Early, CommodityCategory.Religious), // Idols
-        new CommodityData(4.0f, 0.005f, 0.002f, CommodityFlag.Integral, GameTier.Early, CommodityCategory.Religious), // Texts
-        new CommodityData(15.0f, 0.002f, 0.003f, CommodityFlag.Integral, GameTier.Late, CommodityCategory.Religious), // Relics
+        new CommodityData(8f, 0.003f, 0.004f, CommodityFlag.Integral, GameTier.Early, CommodityCategory.Religious), // Idols
+        new CommodityData(25f, 0.005f, 0.002f, CommodityFlag.Integral, GameTier.Mid, CommodityCategory.Religious), // Texts
+        new CommodityData(500f, 0.002f, 0.003f, CommodityFlag.Integral, GameTier.Late, CommodityCategory.Religious), // Relics
     ];
 
     public static CommodityFlag Flags(this Commodity commodity) => Data[commodity].Flag;
